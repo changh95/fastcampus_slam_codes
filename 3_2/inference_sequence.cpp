25a6f74cb990397fa8a97feacfaf00e8c264e779
Hyunggi Chang <hyunggi.chang95@gmail.com>
2023-10-16 17:14:18 +0900

    [3_2] Updating code for sequential matching

--- 3_2/inference_sequence.cpp
+++ 3_2/inference_sequence.cpp
@@ -10,14 +10,13 @@
 
 int main(int argc, char** argv){
     if (argc != 5) {
-        std::cerr << "./superpointglue_sequence config_path model_dir image_folder_absolutely_path output_folder_path" << std::endl;
+        std::cerr << "./superpointglue_sequence config_path image_folder_absolutely_path" << std::endl;
         return 0;
     }
 
     std::string config_path = argv[1];
-    std::string model_dir = argv[2];
+    std::string model_dir = "weights";
     std::string image_path = argv[3];
-    std::string output_path = argv[4];
     std::vector<std::string> image_names;
     GetFileNames(image_path, image_names);
     Configs configs(config_path, model_dir);
@@ -36,35 +35,19 @@ int main(int argc, char** argv){
         return 0;
     }
     std::cout << "SuperPoint and SuperGlue inference engine build success." << std::endl;
-
-    Eigen::Matrix<double, 259, Eigen::Dynamic> feature_points0;
-    cv::Mat image0 = cv::imread(image_names[0], cv::IMREAD_GRAYSCALE);
-    if(image0.empty()) {
-        std::cerr << "First image in the image folder is empty." << std::endl;
-        return 0;
-    }
-    cv::resize(image0, image0, cv::Size(width, height));
-    std::cout << "First image size: " << image0.cols << "x" << image0.rows << std::endl;
-    if (!superpoint->infer(image0, feature_points0)) {
-        std::cerr << "Failed when extracting features from first image." << std::endl;
-        return 0;
-    }
-    std::vector<cv::DMatch> init_matches;
-    superglue->matching_points(feature_points0, feature_points0, init_matches);
-    std::string mkdir_cmd = "mkdir -p " + output_path;
-    system(mkdir_cmd.c_str());
-
     for (int index = 1; index < image_names.size(); ++index) {
-        Eigen::Matrix<double, 259, Eigen::Dynamic> feature_points1;
+        Eigen::Matrix<double, 259, Eigen::Dynamic> feature_points0, feature_points1;
         std::vector<cv::DMatch> superglue_matches;
+
+        cv::Mat image0 = cv::imread(image_names[index-1], cv::IMREAD_GRAYSCALE);
         cv::Mat image1 = cv::imread(image_names[index], cv::IMREAD_GRAYSCALE);
-        if(image1.empty()) continue;
+        cv::resize(image0, image0, cv::Size(width, height));
         cv::resize(image1, image1, cv::Size(width, height));
 
         std::cout << "Second image size: " << image1.cols << "x" << image1.rows << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
-        if (!superpoint->infer(image1, feature_points1)) {
-            std::cerr << "Failed when extracting features from second image." << std::endl;
+        if (!superpoint->infer(image0, feature_points0) || !superpoint->infer(image1, feature_points1)) {
+            std::cerr << "Failed when extracting features." << std::endl;
             return 0;
         }
         superglue->matching_points(feature_points0, feature_points1, superglue_matches);
@@ -85,7 +68,6 @@ int main(int argc, char** argv){
             keypoints1.emplace_back(x, y, 8, -1, score);
         }
         VisualizeMatching(image0, keypoints0, image1, keypoints1, superglue_matches, match_image, duration.count());
-        cv::imwrite(output_path + "/" + std::to_string(index) + ".png", match_image);
     }
   
   return 0;
