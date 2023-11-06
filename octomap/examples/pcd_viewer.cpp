4412175aab7694c670e29c0ae300deb628ff1437
Hyunggi Chang <hyunggi.chang95@gmail.com>
2023-11-07 04:27:58 +0900

    [bonxai] typo

--- octomap/examples/pcd_viewer.cpp
+++ octomap/examples/pcd_viewer.cpp
@@ -29,7 +29,7 @@ int main (int argc, char* argv[])
 {
   if (argc != 2)
   {
-    std::cerr << "./pcd_viewer [path_to_pcd_file] << std::endl";
+    std::cerr << "./pcd_viewer [path_to_pcd_file]" << std::endl;
     return -1;
   }
   
