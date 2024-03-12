455339d08883c42cb94f69162f12aa4be1dd5aa6
Hyunggi Chang <hyunggi.chang95@gmail.com>
2024-03-12 10:44:21 +0900

    fix compilation issue for sophus_demo
    - Due to downgrading OS version in the previous patch, the default Eigen
      version changed from 3.4.0 -> 3.3.7.
    - Eigen 3.3.7 does not support compiling 'Eigen::Vector<double, 6'.
      Instead, we must use 'Eigen::Matrix<double, 6, 1'.
    - Changed the code accordingly

--- 2_6/examples/sophus_demo.cpp
+++ 2_6/examples/sophus_demo.cpp
@@ -13,22 +13,27 @@ int main()
     // Convert rotation vector into SO(3) via exponential map
     Sophus::SO3d rand_R = Sophus::SO3d::exp(rand_vec3);
     std::cout << "rand_R:\n" << rand_R.matrix() << std::endl;
-    
+
     // Convert back to rotation vector via log map
     Eigen::Vector3d log_rand_R = rand_R.log();
     std::cout << "log_rand_R: " << log_rand_R.transpose() << std::endl << std::endl;
-    
+
     // SE(3) uses
     // Generate a random vector
-    Eigen::Vector<double, 6> rand_vec6 = Eigen::Vector<double, 6>::Random() / 100.0;
+
+    // If you use Eigen 3.4.0 or higher, the code below will work (and is better to read!)
+    // Eigen::Vector<double, 6> rand_vec6 = Eigen::Vector<double, 6>::Random() / 100.0;
+    // If you use Eigen <3.3.9, Vector<double, 6> is not possible, therefore must use Matrix instead.
+    Eigen::Matrix<double, 6, 1> rand_vec6 = Eigen::Matrix<double, 6, 1>::Random() / 100.0;
     std::cout << "rand_vec6: " << rand_vec6.transpose() << std::endl;
 
     // Convert twist vector into SE(3) via exponential map
     Sophus::SE3d rand_T = Sophus::SE3d::exp(rand_vec6);
     std::cout << "rand_T:\n" << rand_T.matrix() << std::endl;
-    
+
     // Convert back to twist vector via log map
-    Eigen::Vector<double, 6> log_rand_T = rand_T.log();
+    // Eigen::Vector<double, 6> log_rand_T = rand_T.log();
+    Eigen::Matrix<double, 6, 1> log_rand_T = rand_T.log();
     std::cout << "log_rand_T: " << log_rand_T.transpose() << std::endl;
 
 
@@ -46,3 +51,4 @@ int main()
 
     return 0;
 }
+
